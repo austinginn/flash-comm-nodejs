2609477c1f45fd51dbc74df465bc2697ab79a6e6
Austin Ginn <austinleeginn@gmail.com>
2023-11-20 10:44:39 -0500

    added test script for gpio footswitch

---
+++ tests/footswitch.mjs
@@ -0,0 +1,33 @@
+//GPIO for raspberry pi
+import gpio from "rpi-gpio";
+import { promisify } from "util";
+
+//Async gpio
+const setupAsync = promisify(gpio.setup);
+const writeAsync = promisify(gpio.write);
+const readAsync = promisify(gpio.read);
+
+
+//init footswitch
+await initFootSwitch();
+
+//Untested
+//Footswitch callback
+gpio.on('change', async (channel, value) => {
+    console.log("GPIO: Foot switch pressed");
+    console.log(`GPIO: ${channel}, ${value}`);
+    //send data to server if connected
+});
+
+
+//Untested
+//initialize foot switch
+async function initFootSwitch() {
+    try {
+        await setupAsync(17, gpio.DIR_IN, gpio.EDGE_BOTH);
+        console.log("GPIO: Foot switch initialized");
+    } catch (error) {
+        console.log("Error initializing foot switch");
+        console.log(error);
+    }
+}
