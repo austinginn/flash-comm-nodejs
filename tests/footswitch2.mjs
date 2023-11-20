c22ecc04ecafde18ced1f0365076b6f41b56f04c
Austin Ginn <austinleeginn@gmail.com>
2023-11-20 11:40:35 -0500

    test

---
+++ tests/footswitch2.mjs
@@ -0,0 +1,29 @@
+import { Gpio } from "onoff";
+import { promisify } from "util";
+
+//pin from arg
+const args = process.argv.slice(2);
+let PIN = 17;
+
+// Check if there are arguments
+if (args.length === 0) {
+    console.log('No arguments provided.');
+    let PIN = args[0];
+} else {
+    // Output the provided arguments
+    console.log('Arguments:', args);
+}
+
+
+const button = new Gpio(17, 'in', 'both');
+
+button.watch((err, value) => {
+    if (err) {
+        throw err;
+    }
+    console.log(value);
+});
+
+process.on('SIGINT', _ => {
+    button.unexport();
+});
